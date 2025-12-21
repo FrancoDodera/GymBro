8845b01cdcff6d93df0b7b128f2d90f461a7c6e0
franco.dodera <frando.dodera@cunix.net>
2025-12-21 19:37:43 -0300

    iA log

--- src/pages/cliente/AIPlanGenerator.jsx
+++ src/pages/cliente/AIPlanGenerator.jsx
@@ -38,6 +38,8 @@ const AIPlanGenerator = () => {
     const loadEjercicios = async () => {
         try {
             const ejercicios = await ejerciciosService.getAll();
+            console.log('[AIPlanGenerator] Ejercicios cargados:', ejercicios.length);
+            console.log('[AIPlanGenerator] Sample ejercicio:', ejercicios[0]);
             setEjerciciosDisponibles(ejercicios);
         } catch (error) {
             console.error('Error loading ejercicios:', error);
